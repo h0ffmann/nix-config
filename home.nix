3937d1357b8c8a80a951f86bec2af3975085be63
M.Hoffmann <hoffmann@poli.ufrj.br>
2024-12-31 10:36:09 -0300

    updates
    Signed-off-by: M.Hoffmann <hoffmann@poli.ufrj.br>

--- home.nix
+++ home.nix
@@ -5,11 +5,13 @@
   home.homeDirectory = "/home/h0ffmann";
   nixpkgs.config.allowUnfree = true;
   home.packages = with pkgs; [
-    # Add your user-specific packages here
     brave
     zoom-us
     xclip
-    # warp-terminal
+    nil # Add this line - Nix Language Server
+    nixpkgs-fmt # Add this for formatting
+    statix # Nix Linting
+    just
   ];
 
   programs.bash = {
@@ -23,50 +25,12 @@
     # set some aliases, feel free to add more or remove some
     shellAliases = {
       k = "kubectl";
-      update = "sudo nixos-rebuild switch"; 
-      dumpnix = "sudo cat /etc/nixos/configuration.nix && sudo cat /etc/nixos/home.nix"; 
+      nixedit = "cd /etc/nixos && code .";
       urldecode = "python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";
       urlencode = "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";
     };
   };
-  programs.vscode = {
-    enable = true;
-    package = pkgs.vscode;
-    extensions = with pkgs.vscode-extensions; [
-      # Nix support
-      bbenoist.nix
-      jnoortheen.nix-ide
 
-      # Just support
-      skellock.just
-
-      # Haskell support
-      haskell.haskell
-      justusadam.language-haskell
-
-      # Scala support
-      scala-lang.scala
-      scalameta.metals
-
-      # Other useful extensions
-      vscodevim.vim  # If you use Vim mode
-      ms-python.python  # Python support
-      rust-lang.rust-analyzer  # Rust support
-      golang.go  # Go support
-      eamodio.gitlens  # Git integration
-      
-      # Theme (optional)
-      pkief.material-icon-theme
-    ];
-    userSettings = {
-      "editor.fontFamily" = "Liberation Mono";
-      "editor.fontSize" = 14;
-      "editor.formatOnSave" = true;
-      "files.autoSave" = "onFocusChange";
-      "workbench.colorTheme" = "Default Dark+";
-      "workbench.iconTheme" = "material-icon-theme";
-    };
-  };
   programs.git = {
     enable = true;
     userName = "M.Hoffmann";
@@ -74,7 +38,7 @@
   };
   dconf.settings = {
     "org/gnome/settings-daemon/plugins/media-keys" = {
-      "custom-keybindings" = ["/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"];
+      "custom-keybindings" = [ "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/" ];
     };
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
       "binding" = "F12";
