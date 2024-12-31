b70891b2f18c27df5fb2a6d93ca2ca01a9725856
Matheus Hoffmann <hoffmann@poli.ufrj.br>
2024-12-31 13:15:30 -0300

    updates (#2)
    Signed-off-by: M.Hoffmann <hoffmann@poli.ufrj.br>

--- dumpnix.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo "=== configuration.nix ==="
-cat /etc/nixos/configuration.nix 
-echo -e "\n=== home.nix ==="
-cat /etc/nixos/home.nix 
-echo -e "\n=== flake.nix ==="
-cat /etc/nixos/flake.nix
--- flake.nix
+++ flake.nix
@@ -1,5 +1,6 @@
 {
   description = "A simple NixOS flake";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     home-manager = {
@@ -7,21 +8,39 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = { self, nixpkgs, home-manager }: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+
+  outputs = { self, nixpkgs, home-manager }:
+    let
       system = "x86_64-linux";
-      modules = [
-        # Import your main configuration
-        {
-          imports = [
-            ./configuration.nix
-            ./hardware-configuration.nix
-            ./vscode.nix
-          ];
-        }
-        # Import home-manager module
-        home-manager.nixosModules.home-manager
-      ];
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          {
+            imports = [
+              ./configuration.nix
+              ./hardware-configuration.nix
+              ./vscode.nix
+            ];
+          }
+          home-manager.nixosModules.home-manager
+        ];
+      };
+
+      # checks.${system} = {
+      #   # Check if the configuration builds
+      #   build = self.nixosConfigurations.nixos.config.system.build.toplevel;
+
+      #   # Add statix checks
+      #   statix = pkgs.runCommand "statix-check" { } ''
+      #     ${pkgs.statix}/bin/statix check ${self} --ignore repeated-keys
+      #     touch $out
+      #   '';
+      # };
+
+      # Optional: add formatter
+      formatter.${system} = pkgs.nixpkgs-fmt;
     };
-  };
 }
